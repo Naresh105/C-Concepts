3d7e12a78d4e3df93188361a12260e6b78859a0c
Naresh105 <nareshkuruva616@gmail.com>
2025-01-24 23:44:29 +0530

    Add files via upload

---
+++ Single_Leve_inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.NetworkInformation;
+
+
+   
+
+namespace SingleLevelInheritance
+{
+   //parent class
+    class Vehicle
+    {
+        public void WhiteCar()
+        {
+            Console.WriteLine("This White car.");
+        }
+    }
+
+    // Child Class
+    class Car : Vehicle
+    {
+        public void RedCar()
+        {
+            Console.WriteLine("This is Red Car.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of the Child Class
+            Car myCar = new Car();
+
+            // Access method of the Parent Class
+            myCar.WhiteCar();
+
+            // Access method of the Child Class
+            myCar.RedCar();
+
+            Console.ReadLine();
+        }
+    }
+}
