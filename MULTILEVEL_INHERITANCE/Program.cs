7a0c565d9c46e3c15b570042d9bde82f510ae13a
Naresh105 <nareshkuruva616@gmail.com>
2025-01-25 00:12:33 +0530

    Add files via upload

---
+++ MULTILEVEL_INHERITANCE/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MULTILEVEL_INHERITANCE
+{
+   
+            class Vehicle  // Base class
+        {
+            public void speed()
+            {
+                Console.WriteLine("Vehicle is running on the road........");
+            }
+        }
+
+        class Car : Vehicle  // Derived class from Vehicle
+        {
+            public void Break()
+            {
+                Console.WriteLine("Car Stops...");
+            }
+        }
+
+        class NanoCar : Car  // Derived class from Car (and indirectly from Vehicle)
+        {
+            public void Clutch()
+            {
+                Console.WriteLine("temporarily disconnect the engine power from the transmission...");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                NanoCar Move = new NanoCar();
+                Move.speed();    // Inherited from Vehicle
+                Move.Break();  // Inherited from Car
+                Move.Clutch();   // Defined in NanoCar class
+            }
+        }
+
+    }
+
