0da749276a78d727e4a8809a32ffd1c0c4e3affb
Naresh105 <nareshkuruva616@gmail.com>
2025-01-25 17:32:36 +0530

    Add files via upload

---
+++ parameterised_Constructor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parameterised_Constructor
+{
+   
+       public class Person
+     {
+            private string name;
+            private int age;
+
+            // Default constructor
+            public Person()
+            {
+                name = "Unknown";
+                age = 0;
+            }
+
+            // Parameterized constructor
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+           public static void Main(String[] args)
+            {
+            Person person = new Person();   
+                Console.WriteLine("Name: " + person.name);
+                Console.WriteLine("Age: " + person.age);
+            }
+        }
+    }
+
