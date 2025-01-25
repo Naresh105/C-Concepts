5d63b3e8a650ae0acdb020b21b1555f2b4a5fd6c
Naresh105 <nareshkuruva616@gmail.com>
2025-01-25 17:34:56 +0530

    Add files via upload

---
+++ Override_using_Abstract/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Override_using_Abstract
+{
+   
+            public abstract class Shape
+        {
+            public abstract double GetArea();
+        }
+
+public class Circle : Shape
+        {
+            private double _radius;
+
+            public Circle(double radius)
+            {
+                _radius = radius;
+            }
+
+            public override double GetArea()
+            {
+                return Math.PI * _radius * _radius;
+            }
+        
+        static void Main(string[] args) { 
+                Shape myShape = new Circle(5);
+                Console.WriteLine(myShape.GetArea()); // Output: Area of the circle
+
+        }
+}
+}
