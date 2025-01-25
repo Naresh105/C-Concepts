8f1825a076f1642f10168319589db099eb353804
Naresh105 <nareshkuruva616@gmail.com>
2025-01-25 17:22:19 +0530

    Add files via upload

---
+++ Default_Constuctor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Default_Constuctor
+{
+   
+
+        class Person
+        {
+            private string name;
+
+            // Default constructor
+            public Person()
+            {
+                name = "Unknown"; // Default value for the name
+                Console.WriteLine("Default constructor called.");
+            }
+        // Method
+            public void Display()
+            {
+                Console.WriteLine("Name: " + name);
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Create an object using the default constructor
+                Person person = new Person();
+                person.Display(); // Output: Name: Unknown
+            }
+        }
+    }
+
