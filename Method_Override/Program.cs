8b5109f5730191a4949d4902a52480db3f229963
Naresh105 <nareshkuruva616@gmail.com>
2025-01-25 17:34:18 +0530

    Add files via upload

---
+++ Method_Override/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Method_Override
+{
+    internal class Program { }
+    
+            public class Animal
+        {
+            public virtual void Speak()
+            {
+                Console.WriteLine("The animal makes a sound.");
+            }
+        }
+        
+public class Dog : Animal
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("The dog barks.");
+            }
+        static void Main(string[] args) { 
+            Animal myAnimal = new Dog();
+                myAnimal.Speak(); // Output: "The dog barks."
+
+        }
+
+}}
