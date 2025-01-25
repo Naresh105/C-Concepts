1bcc7d0406414036db236aaf5df77b1c61cb010e
Naresh105 <nareshkuruva616@gmail.com>
2025-01-25 17:20:45 +0530

    Add files via upload

---
+++ Method_using_ReturnType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_using_ReturnType
+{
+    public class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                int result = AddNumbers(5, 10);
+                Console.WriteLine("The sum is: " + result); 
+            // Output: The sum is: 15
+            }
+
+            static int AddNumbers(int num1, int num2)
+            {
+                return num1 + num2;
+            }
+        }
+
+    }
+
