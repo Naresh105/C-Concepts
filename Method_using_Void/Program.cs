c35f45d1031f5f2f9a7d01aa613830892dc0c70a
Naresh105 <nareshkuruva616@gmail.com>
2025-01-25 17:21:22 +0530

    Add files via upload

---
+++ Method_using_Void/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_using_Void
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+           
+            
+                PrintMessage("Hello, world!"); // Call the method
+            }
+
+            static void PrintMessage(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+    }
+
