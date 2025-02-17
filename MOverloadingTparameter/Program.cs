723f5e775dc09c9e940252604106822d6942d064
Naresh105 <nareshkuruva616@gmail.com>
2025-02-17 14:57:05 +0530

    Add files via upload

---
+++ MOverloadingTparameter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOverloadingTparameter
+{
+    internal class Program
+    {
+       
+        public void Print(int a)
+        {
+            Console.WriteLine("Integer: " + a);
+        }
+
+        public void Print(double a)
+        {
+            Console.WriteLine("Double: " + a);
+        }
+
+        static void Main()
+        {
+            Program obj = new Program();
+            obj.Print(10);    // Calls Print(int)
+            obj.Print(10.5);  // Calls Print(double)
+        }
+
+
+      
+    }
+}
---
+++ MOverloadingTparameter/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MOverloadingTparameter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MOverloadingTparameter")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba16faa1-513d-4569-b3b8-3bb96887d583")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
