b4a63d28e37b4c7944151a9b26a93828461c7920
Naresh105 <nareshkuruva616@gmail.com>
2025-02-17 14:26:49 +0530

    Add files via upload

---
+++ MOverloadingNOfPParameter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOverloadingNOfPParameter
+{
+    internal class Program
+    {
+
+
+
+        // Method with one parameter
+        public void Display(int a)
+        {
+            Console.WriteLine("Integer: " + a);
+        }
+
+        // Overloaded method with two parameters
+        public void Display(int a, int b)
+        {
+            Console.WriteLine("Sum: " + (a + b));
+        }
+
+        static void Main()
+        {
+            Program obj = new Program();
+            obj.Display(10);      // Calls first method
+            obj.Display(10, 20);  // Calls overloaded method
+        }
+
+
+
+    }
+}
