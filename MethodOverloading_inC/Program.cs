31762e22074cb3dc9d05771b172823a76a6d20c9
Naresh105 <nareshkuruva616@gmail.com>
2025-02-17 15:28:41 +0530

    Add files via upload

---
+++ MethodOverloading_inC/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading_inC
+{
+    internal class Program
+    {
+
+        class OverloadingExample
+        {
+            // Method with one parameter
+            public void Display(int a)
+            {
+                Console.WriteLine("Integer: " + a);
+            }
+
+            // Method with two parameters
+            public void Display(int a, double b)
+            {
+                Console.WriteLine("Integer: " + a + ", Double: " + b);
+            }
+
+            // Method with a different type of parameter
+            public void Display(string message)
+            {
+                Console.WriteLine("Message: " + message);
+            }
+
+            static void Main()
+            {
+                OverloadingExample obj = new OverloadingExample();
+
+                obj.Display(10);
+                obj.Display(10, 20.5);
+                obj.Display("Hello, C#");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ MethodOverloading_inC/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MethodOverloading_inC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MethodOverloading_inC")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("914417f1-5ecf-47d0-acef-8e0dabc19274")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
